22d5f44b8887a6df8341bd38813ad9c90881b2aa
cngsym <seyma.ibis@bil.omu.edu.tr>
2011-06-20 16:15:23 +0300

    deneme

---
+++ bolge.py
@@ -0,0 +1,15 @@
+# -*- coding: cp1254 -*-
+def bolge(x, y):
+    
+    if x >0 and y > 0:
+        print 'I.Bölge'
+        
+    if x < 0 and y >0:
+       print 'II.Bölge'
+        
+    if x < 0 and y <0:
+       print 'III.Bölge'
+    
+    if x > 0 and y <0:
+       print 'IV.Bölge'
+    
---
+++ foyler/boyalı.py
@@ -0,0 +1,24 @@
+import turtle
+t=turtle
+t.pen(fillcolor="yellow")
+t.begin_fill()
+for i in range(36):
+    t.left(10)
+    t.forward(5)
+t.end_fill()
+t.right(90)
+t.forward(10)
+t.pen(fillcolor="pink")
+t.begin_fill()
+for i in range(27):
+    t.left(10)
+    t.forward(5)
+t.end_fill()
+t.pen(fillcolor="red")
+t.begin_fill()
+for i in range(27):
+    t.left(10)
+    t.forward(5)
+t.end_fill()
+
+    
