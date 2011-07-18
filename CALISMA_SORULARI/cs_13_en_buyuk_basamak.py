34f980400d5e16bd7245ee816cf2a5aa6a549026
cngsym <seyma.ibis@bil.omu.edu.tr>
2011-07-18 21:28:56 +0300

    girilen sayının enbuyuk basamagı

---
+++ CALISMA_SORULARI/cs_13_en_buyuk_basamak.py
@@ -0,0 +1,17 @@
+#!usr/bin/env python
+# -*- coding utf8: -*-
+
+def en_buyuk_basamak(sayi):
+    a = repr(sayi)
+    b = 0
+    
+    for i in a:
+        i = int(i)
+        
+        if i >= b : b = i
+         
+        else: pass
+         
+    return b
+        
+          
